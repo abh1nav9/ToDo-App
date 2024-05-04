699793e7bd929f8b07c88eb50995005bb654b9af
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-05-04 19:22:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>ToDo App</h1>
+      <AddTodo />
+      <Todos />
+      
+    </>
+  )
+}
+
+export default App
